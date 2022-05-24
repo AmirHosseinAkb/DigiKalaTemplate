358d7f1561a7f892bae83b1c8395bf2e914904ba
AmirHosseinAkb <amirbvk1@gmail.com>
2022-05-24 21:34:58 +0430

    ConfirmCultureCookieHandlerMiddleware

---
+++ DigiKala.Core/Middlewares/CultureCookieHandlerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiKala.Core.Middlewares
+{
+    public class CultureCookieHandlerMiddleware
+    {
+        public CultureCookieHandlerMiddleware(RequestDelegate next)
+        {
+            Next = next;
+        }
+        private RequestDelegate Next { get; }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var cultureInfo =
+                new System.Globalization.CultureInfo("fa-IR");
+            System.Threading.Thread.CurrentThread.CurrentCulture = cultureInfo;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            await Next(httpContext);
+        }
+    }
+}
