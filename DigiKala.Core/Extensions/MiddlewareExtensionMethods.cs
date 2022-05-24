fc8311b44148277d5cefc3f8a81f1b44d90f5412
AmirHosseinAkb <amirbvk1@gmail.com>
2022-05-24 22:27:35 +0430

    ConfirmCultureCookieExtensionMethod

--- DigiKala.Core/Extensions/MiddlewareExtensionMethods.cs
+++ DigiKala.Core/Extensions/MiddlewareExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace DigiKala.Core.Extensions
 {
-    internal class MiddlewareExtensionMethods
+    public static class MiddlewareExtensionMethods
     {
+        public static IApplicationBuilder UseCultureCookie(this IApplicationBuilder app)
+            => app.UseMiddleware<Middlewares.CultureCookieHandlerMiddleware>();
     }
 }
