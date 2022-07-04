e0454ebdd9e86b5494a8c1fdbba16e53a269f7c0
AmirHosseinAkb <amirbvk1@gmail.com>
2022-07-04 17:47:46 +0430

    AddDeletedUsersPage

---
+++ DigiKala/Pages/Admin/Users/DeletedUsersList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigiKala.Pages.Admin.Users
+{
+    public class DeletedUsersListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
